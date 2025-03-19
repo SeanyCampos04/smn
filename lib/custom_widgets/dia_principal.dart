31239480cdf7aef4c16ef50267829db4da410134
SeanyCampos04 <21690072@tecvalles.mx>
2025-03-19 09:25:46 -0600

    Agregué responsividad, icono de informacion y comenzamos con WidgetDia

--- lib/custom_widgets/dia_principal.dart
+++ lib/custom_widgets/dia_principal.dart
@@ -1,33 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:smn/custom_widgets/texto_gris_negrita.dart';
 import 'package:smn/models/modelo_pronostico.dart';
 
 class DiaPrincipal extends StatelessWidget {
   final ModeloPronostico dia;
   DiaPrincipal({super.key, required this.dia});
 
+  Widget Separador() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+          border: Border(
+        right: BorderSide(
+          color: Colors.black54,
+          width: 2,
+        ),
+      )),
+      child: SizedBox(
+        width: 40,
+        height: 40,
+      ),
+    );
+  }
+
+  Widget Elemento(String texto, IconData icono, String medida) {
+    return Column(
+      children: [
+        Text(texto),
+        Row(
+          children: [Icon(icono), Text(medida)],
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-      Text(dia.desciel),
-      Container(
-        child: Row (mainAxisAlignment: MainAxisAlignment
-          Icon(
-            Icons.sunny, 
-            color: Colors.amber, 
-            size: 80,
+        Text(dia.desciel),
+        Container(
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Icon(
+              Icons.sunny,
+              color: Colors.amber,
+              size: 80,
             ),
-          Text(
-            dia.tmax, 
-            style: TextStyle (
-              fontSize: 40.0
-              color: Colors.blueAccent,
+            Text(
+              dia.tmax,
+              style: TextStyle(
+                fontSize: 70.0,
+                color: Colors.redAccent,
               ),
+            ),
+            Text(
+              "/${dia.tmin} ºC",
+              style: TextStyle(
+                fontSize: 35.0,
+                color: Colors.blueAccent,
               ),
-          Text("/${dia.tmin} C")
-        ],)
-      )
-      ]
+            ),
+          ]),
+        ),
+        MediaQuery.of(context).size.width <= 600
+            ? Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  children: [
+                    TextoGrisNegrita(
+                        texto: "Probabilidad de lluvia:", medida: dia.probprec),
+                    TextoGrisNegrita(
+                        texto: "Velocidad del viento:", medida: dia.velvien),
+                    TextoGrisNegrita(
+                        texto: "Dirección del viento:", medida: dia.dirvienc),
+                    TextoGrisNegrita(
+                        texto: "Ráfaga de viento:", medida: dia.raf),
+                  ],
+                ),
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Elemento("Lluvia", Icons.water_drop, "${dia.prec}lts/m2"),
+                  Separador(),
+                  Elemento("Probabilidad de lluvia", Icons.cloudy_snowing,
+                      "${dia.probprec}lts/m2"),
+                  Separador(),
+                  Elemento("Dirección del viento", Icons.arrow_outward,
+                      "${dia.dirvienc}kms/h"),
+                  Separador(),
+                  Elemento("Velocidad del viento", Icons.speed_outlined,
+                      "${dia.velvien}kms/h"),
+                ],
+              )
+      ],
     );
   }
 }
--- lib/pages/pagina_inicial.dart
+++ lib/pages/pagina_inicial.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:smn/custom_widgets/dia_principal.dart';
+import 'package:smn/custom_widgets/widget_dia.dart';
+
 import 'package:smn/models/modelo_municipio.dart';
 import 'package:smn/models/modelo_pronostico.dart';
 import 'package:smn/providers/provider_lista_municipios.dart';
 import 'package:smn/providers/provider_pronosticos.dart';
 import 'buscar_municipio.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:intl/intl.dart';
 
 class PaginaInicial extends StatefulWidget {
   const PaginaInicial({super.key});
@@ -24,30 +25,27 @@ class _PaginaInicialState extends State<PaginaInicial> {
   String _hoy = DateFormat("dd MMMM", "es_ES").format(DateTime.now());
   String _ciudad = "Cargando ciudad...";
 
-
   @override
   void initState() {
     super.initState();
     _getLocation();
 
-    Future.microtask((){
-      Provider.of<ProviderPronosticos>(
-        context, listen: false)
-        .cargaPronosticos();
-    }
-    );
-
+    Future.microtask(() {
+      Provider.of<ProviderPronosticos>(context, listen: false)
+          .cargaPronosticos();
+    });
   }
-
 
   Future<void> _getLocation() async {
     _isGPSEnabled = await Geolocator.isLocationServiceEnabled();
+
     if (!_isGPSEnabled) {
       setState(() {
         _status = "El servicio de ubicación está desactivado";
       });
-     return;
-  }
+      return;
+    }
+
     LocationPermission permisoUbicacion = await Geolocator.checkPermission();
 
     if (permisoUbicacion == LocationPermission.denied) {
@@ -73,6 +71,7 @@ class _PaginaInicialState extends State<PaginaInicial> {
     Position posicion = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
+
     List<Placemark> listaDeUbicaciones =
         await placemarkFromCoordinates(posicion.latitude, posicion.longitude);
 
@@ -88,21 +87,24 @@ class _PaginaInicialState extends State<PaginaInicial> {
 
   @override
   Widget build(BuildContext context) {
+    bool esTelefono = MediaQuery.of(context).size.width <= 600;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber,
-        title: Text('Pronóstico del Tiempo por Municipios',
-        style: TextStyle(fontSize: 18),
+        title: Text(
+          'Pronóstico del Tiempo por Municipios',
+          style: TextStyle(fontSize: 18),
         ),
       ),
       body: Column(
         children: <Widget>[
           GestureDetector(
-            onTap: ()=>Navigator.push(
+            onTap: () => Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context)=> BuscarMunicipio(),
-                ),
+                builder: (context) => BuscarMunicipio(),
+              ),
             ),
             child: Container(
               padding: EdgeInsets.symmetric(
@@ -110,57 +112,123 @@ class _PaginaInicialState extends State<PaginaInicial> {
                 vertical: 16,
               ),
               decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                )),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                      ),
-                    Text(
-                      'Buscar municipio',
+                  border: Border.all(
+                color: Colors.grey,
+              )),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    'Buscar municipio',
                     style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                ),
+                  ),
+                ],
+              ),
             ),
           ),
-          SizedBox( height: 20,),
+          SizedBox(
+            height: 20,
+          ),
           Text(
             _ciudad,
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 24,
             ),
-            ),
-            Text(_hoy),
-            Consumer<ProviderPronosticos>(
-              builder: 
-              (context, proveedor_de_dias, child) {
-                if(proveedor_de_dias.estaCargando){
-                  return Center(
-                    child: CircularProgressIndicator(),
-                    );
-                }
-                if (proveedor_de_dias.pronosticos.isEmpty) {
-                return Center(
-                  child: Text("No hay pronósticos disponibles"),
-                );
-                }
-                ModeloPronostico diaPrincipal = proveedor_de_dias.pronosticos[0];
-                return Expanded(
-                  child: SingleChildScrollView(
-                 child: Column(
+          ),
+          Text(_hoy),
+          Consumer<ProviderPronosticos>(
+              builder: (context, proveedor_de_dias, child) {
+            if (proveedor_de_dias.estaCargando) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (proveedor_de_dias.pronosticos.isEmpty) {
+              return Center(
+                child: Text("No hay pronósticos diponibles"),
+              );
+            }
+
+            ModeloPronostico diaPrincipal = proveedor_de_dias.pronosticos[0];
+
+            return Expanded(
+              child: SingleChildScrollView(
+                child: Column(
                   children: [
-                    DiaPrincipal(dia: diaPrincipal)
+                    DiaPrincipal(dia: diaPrincipal),
+                    ExpansionTile(
+                      title: Text(""),
+                      trailing: Icon(
+                        Icons.info,
+                        size: 40,
+                        color: Colors.amber,
+                      ),
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(20.0),
+                          decoration: BoxDecoration(
+                            color: Color(0xfffcf8e3),
+                            border: Border.all(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Temp max: ',
+                                    style: TextStyle(
+                                        color: Colors.redAccent,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text("Puede variar de 1° a 3°C")
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Temp min: ',
+                                    style: TextStyle(
+                                        color: Colors.blueAccent,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text("Puede variar +/- 1°C")
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    LayoutBuilder(builder: (context, condicionantes) {
+                      return Flex(
+                        direction: esTelefono ? Axis.vertical : Axis.horizontal,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: List.generate(4, (index) {
+                          return SizedBox(
+                            width: esTelefono
+                                ? condicionantes.maxWidth
+                                : condicionantes.maxWidth / 4,
+                            child: WidgetDia(
+                              pronostico: proveedor_de_dias.pronosticos[index],
+                              index: index,
+                            ),
+                          );
+                        }),
+                      );
+                    }),
                   ],
-                 ) 
-                  )
-                );
-              }
+                ),
               ),
+            );
+          }),
         ],
       ),
     );
