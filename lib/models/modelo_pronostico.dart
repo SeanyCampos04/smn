ad0cbb3ba197d561af00acfd50364aa3dd845159
SeanyCampos04 <21690072@tecvalles.mx>
2025-03-21 09:24:13 -0600

    Se modificó el Widget Dia, se agregaron datos y se modificó el modelo pronostico, se agregó fecha y se modificó el servicio pronostico para agregar el campo fecha al modelo

--- lib/models/modelo_pronostico.dart
+++ lib/models/modelo_pronostico.dart
@@ -1,5 +1,5 @@
 /*
-"nes": "San Luis Potosí",
+    "nes": "San Luis Potosí",
     "nmun": "Ciudad Valles",
     "tmax": "18.8",
     "tmin": "10.2",
@@ -9,8 +9,7 @@
     "velvien": "3.9",
     "dirvienc": "Noroeste",
     "raf": "10.4"
-
-  */
+ */
 
 class ModeloPronostico {
   String nes;
@@ -24,30 +23,41 @@ class ModeloPronostico {
   String dirvienc;
   String raf;
 
-  ModeloPronostico(
-      {required this.nes,
-      required this.nmun,
-      required this.tmax,
-      required this.tmin,
-      required this.desciel,
-      required this.probprec,
-      required this.prec,
-      required this.velvien,
-      required this.dirvienc,
-      required this.raf});
+  String? fecha;
+
+  ModeloPronostico({
+    required this.nes,
+    required this.nmun,
+    required this.tmax,
+    required this.tmin,
+    required this.desciel,
+    required this.probprec,
+    required this.prec,
+    required this.velvien,
+    required this.dirvienc,
+    required this.raf,
+    this.fecha,
+  });
 
   factory ModeloPronostico.fromJson(Map<String, dynamic> json) {
+    //if (json['nes'] == null) {
+    //  nes = '24';
+    //}
+
+    //nes = json['nes'] == null ? '24' : json['nes'];
+
     return ModeloPronostico(
-      nes: json['nes'] ?? "24",
-      nmun: json['nmun'] ?? "13",
-      tmax: json['tmax'] ?? "",
-      tmin: json['tmin'] ?? "",
-      desciel: json['desciel'] ?? "",
-      probprec: json['probprec'] ?? "",
-      prec: json['prec'] ?? "",
-      velvien: json['velvien'] ?? "",
-      dirvienc: json['dirvienc'] ?? "",
-      raf: json['raf'] ?? "",
+      fecha: json['fecha'] ?? "",
+      nes: json['nes'] ?? '24',
+      nmun: json['nmun'] ?? '13',
+      tmax: json['tmax'] ?? '',
+      tmin: json['tmin'] ?? '',
+      desciel: json['desciel'] ?? '',
+      probprec: json['probprec'] ?? '',
+      prec: json['prec'] ?? '',
+      velvien: json['velvien'] ?? '',
+      dirvienc: json['dirvienc'] ?? '',
+      raf: json['raf'] ?? '',
     );
   }
 }
